d02f2981bf0c71553d3e61818db206fa2e9de829
Mahendra <mahendra198927@gmail.com>
2015-06-04 22:35:56 +0530

    Adding Test Project

---
+++ Algorithms/AlgorithmTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+using Algorithms;
+
+namespace AlgorithmTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestSayHi()
+        {
+            HelloWorld helloWorld = new HelloWorld();
+            Console.WriteLine(helloWorld.SayHi());
+            Assert.True("Hi!!" == helloWorld.SayHi());
+        }
+    }
+}
