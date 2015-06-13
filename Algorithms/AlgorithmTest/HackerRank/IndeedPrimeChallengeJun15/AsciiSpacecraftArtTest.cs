d567215adb979129090a3d6cfbefec7f58951f6c
Mahendra <mahendra198927@gmail.com>
2015-06-13 15:32:46 +0530

    Indeed Prime Challenge Ascii Spaceship printing

---
+++ Algorithms/AlgorithmTest/HackerRank/IndeedPrimeChallengeJun15/AsciiSpacecraftArtTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Algorithms.HackerRank.Challenges.IndeedPrimeChallengeJun15;
+
+namespace AlgorithmTest.HackerRank.IndeedPrimeChallengeJun15
+{
+    public class AsciiSpacecraftArtTest
+    {
+        [Fact]
+        public void ExecuteTest()
+        {
+            AsciiSpacecraftArt obj = new AsciiSpacecraftArt();
+            var output1 = obj.Solve(1);
+            var output2 = obj.Solve(2);
+            var output3 = obj.Solve(3);
+            var output4 = obj.Solve(4);
+            var output5 = obj.Solve(5);
+        }
+    }
+}
---
+++ Algorithms/Algorithms/HackerRank/Challenges/IndeedPrimeChallengeJun15/AsciiSpacecraftArt.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.HackerRank.Challenges.IndeedPrimeChallengeJun15
+{
+    public class AsciiSpacecraftArt
+    {
+
+        /*
+          /\
+          ||
+         /||\
+        /:||:\
+        |/||\|
+          **
+          **
+
+          /\
+          ||
+          ||
+         /||\
+        /:||:\
+        |:||:|
+        |/||\|
+          **
+          **
+        */
+
+        public string Solve(int size)
+        {
+            StringBuilder output = new StringBuilder();
+
+            const string FIRST_LINE = @"  /\";
+            const string SECOND_LINE = "  ||";
+            const string THIRD_LINE = @" /||\";
+            const string FOURTH_LINE =@"/:||:\";
+            const string FIFTH_LINE = @"|:||:|";
+            const string SIXTH_LINE = @"|/||\|";
+            const string LAST_LINE =   "  **  ";
+
+            int sizeOfRocket = 1 + size + 1 + size + 1 + 2;
+            
+            for (int i = 0; i < sizeOfRocket; i++)
+            {
+                if(i == 0)
+                {
+                    output.AppendLine(FIRST_LINE);
+                }
+                else if (i <= size)
+                {
+                    output.AppendLine(SECOND_LINE);
+                }
+                else if(i == size + 1)
+                {
+                    output.AppendLine(THIRD_LINE);
+                }
+                else if(i == size + 2)
+                {
+                    output.AppendLine(FOURTH_LINE);
+                }
+                else if(i == sizeOfRocket - 3)
+                {
+                    output.AppendLine(SIXTH_LINE);
+                }
+                else if(i >= sizeOfRocket - 2)
+                {
+                    output.AppendLine(LAST_LINE);
+                }
+                else
+                {
+                    output.AppendLine(FIFTH_LINE);
+                }
+            }
+            return output.ToString();
+        } 
+    }
+}
